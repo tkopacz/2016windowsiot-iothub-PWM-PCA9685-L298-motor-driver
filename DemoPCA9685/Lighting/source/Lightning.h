380a5570e3ff1c71d71fa7c419b1d88093eef20c
tkopacz <tkopacz@tomaszkopacz.com>
2016-07-16 16:08:08 +0200

    Work

--- DemoPCA9685/Lighting/source/Lightning.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Common Lightning macros and declarations
-
-#if defined(LIGHTNING_DLL_EXPORTS) && !defined(LIGHTNING_DLL_API)
-#define LIGHTNING_DLL_API __declspec(dllexport) 
-#elif !defined(LIGHTNING_DLL_API)
-#define LIGHTNING_DLL_API __declspec(dllimport) 
-#endif
