2c5623d99d9db8f6e20e4c0a4e472171f25dea27
tkopacz <tkopacz@tomaszkopacz.com>
2016-07-16 20:13:38 +0200

    servo working as expected

--- DemoPCA9685/DemoPCA9685/MainPage.xaml.cs
+++ DemoPCA9685/DemoPCA9685/MainPage.xaml.cs
@@ -31,7 +31,7 @@ namespace DemoPCA9685
     public sealed partial class MainPage : Page
     {
         private PwmController m_pwmController;
-        private PwmPin m_servo0, m_servo1;
+        private PwmPin[] m_servo = new PwmPin[3];
         private PwmPin m_motor0, m_motor1;
         private DispatcherTimer m_timer;
 
@@ -54,26 +54,109 @@ namespace DemoPCA9685
                 //PCA9685
                 m_pwmController = (await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider()))[0];
                 //24 - 1000
-                m_pwmController.SetDesiredFrequency(50); //For Servo
+                m_pwmController.SetDesiredFrequency(300); //For Servo
+                Debug.WriteLine($"Freq: {m_pwmController.ActualFrequency}");
                 //m_pwmController.SetDesiredFrequency(25); //For L298
                 //m_pwmController.SetDesiredFrequency(1000);
-                m_servo0 = m_pwmController.OpenPin(0);
-                m_servo1 = m_pwmController.OpenPin(1);
+                m_servo[0] = m_pwmController.OpenPin(0);
+                m_servo[1] = m_pwmController.OpenPin(1);
+                m_servo[2] = m_pwmController.OpenPin(3);
+
+                m_servo[2].SetActiveDutyCyclePercentage(0.5);
+                m_servo[2].Start();
+                m_servo[2].SetActiveDutyCyclePercentage(0.2);
+                m_servo[2].SetActiveDutyCyclePercentage(0.9);
+                m_servo[2].SetActiveDutyCyclePercentage(0.5);
+                m_servo[2].Stop();
+
 
                 m_motor0 = m_pwmController.OpenPin(4);
                 m_motor1 = m_pwmController.OpenPin(5);
 
-                //m_servo0.SetActiveDutyCyclePercentage(0.5);
-                //m_servo0.Start();
+                m_servo[0].Start();
+                Servo(0,0.5);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,1);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.9);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.8);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.7);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.6);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.5);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.4);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.3);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.2);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.1);
+                m_servo[0].Stop();
+                m_servo[0].Start();
+                Servo(0,0.0);
+                m_servo[0].Stop();
+
+
+                m_servo[1].Start();
+                Servo(1, 0.5);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 1);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.9);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.8);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.7);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.6);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.5);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.4);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.3);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.2);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.1);
+                m_servo[1].Stop();
+                m_servo[1].Start();
+                Servo(1, 0.0);
+                m_servo[1].Stop();
+
                 //m_servo1.SetActiveDutyCyclePercentage(0.5);
                 //m_servo1.Start();
 
-                m_motor0.SetActiveDutyCyclePercentage(0.5);
-                m_motor0.Start();
-                m_motor1.SetActiveDutyCyclePercentage(0.5);
-                m_motor1.Start();
-                m_motor0.SetActiveDutyCyclePercentage(0.9);
-                m_motor1.SetActiveDutyCyclePercentage(0.9);
+                //m_motor0.SetActiveDutyCyclePercentage(0.5);
+                //m_motor0.Start();
+                //m_motor1.SetActiveDutyCyclePercentage(0.5);
+                //m_motor1.Start();
+                //m_motor0.SetActiveDutyCyclePercentage(0.9);
+                //m_motor1.SetActiveDutyCyclePercentage(0.9);
 
                 var gpioController = await GpioController.GetDefaultAsync();
                 var i2cController = await I2cController.GetDefaultAsync();
@@ -102,6 +185,27 @@ namespace DemoPCA9685
 
         }
 
+        private void Servo(int servo, double v)
+        {
+            var lv = LinMap(v, 0.2, 0.8);
+            m_servo[servo].SetActiveDutyCyclePercentage(lv);
+        }
+        /// <summary>
+        /// Lin map
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="rmin"></param>
+        /// <param name="rmax"></param>
+        /// <returns></returns>
+        private double LinMap(double val,double rmin=0.2, double rmax = 0.8)
+        {
+            if (val < 0 || val > 1 || rmin < 0 || rmin> 1 || rmax<0 || rmax>1) throw new ArgumentException();
+            if (rmin>=rmax ) throw new ArgumentException();
+            var o = rmin + val * (rmax - rmin);
+            return o;
+
+        }
+
         bool m_switch = false;
         private GpioPin m_pin0;
         private GpioPin m_pin1;
@@ -110,14 +214,14 @@ namespace DemoPCA9685
         {
             if (m_switch)
             {
-                //m_servo0.SetActiveDutyCyclePercentage(0.5);
+                //m_servo[0].SetActiveDutyCyclePercentage(0.5);
                 //m_servo1.SetActiveDutyCyclePercentage(0.5);
 
                 m_motor0.SetActiveDutyCyclePercentage(0.5);
                 m_motor1.SetActiveDutyCyclePercentage(0.5);
             } else
             {
-                //m_servo0.SetActiveDutyCyclePercentage(0.2);
+                //m_servo[0].SetActiveDutyCyclePercentage(0.2);
                 //m_servo1.SetActiveDutyCyclePercentage(0.2);
 
                 m_motor0.SetActiveDutyCyclePercentage(0.999);
