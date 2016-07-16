e84d9d0bb88f1a865babee7d7b93f2a5ae5ab662
tkopacz <tkopacz@DOM2.local>
2016-07-16 12:29:10 +0200

    Work

--- DemoPCA9685/DemoPCA9685/MainPage.xaml.cs
+++ DemoPCA9685/DemoPCA9685/MainPage.xaml.cs
@@ -27,8 +27,8 @@ namespace DemoPCA9685
     public sealed partial class MainPage : Page
     {
         private PwmController m_pwmController;
-        private PwmPin m_servo0;
-        private PwmPin m_servo1;
+        private PwmPin m_servo0, m_servo1;
+        private PwmPin m_motor0, m_motor1;
         private DispatcherTimer m_timer;
 
         public MainPage()
@@ -44,20 +44,29 @@ namespace DemoPCA9685
                 //PCA9685
                 m_pwmController = (await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider()))[0];
                 //24 - 1000
-                //m_pwmController.SetDesiredFrequency(50); //For Servo
-                m_pwmController.SetDesiredFrequency(25); //For L298
-                m_servo0 = m_pwmController.OpenPin(4);
-                m_servo1 = m_pwmController.OpenPin(5);
+                m_pwmController.SetDesiredFrequency(50); //For Servo
+                //m_pwmController.SetDesiredFrequency(25); //For L298
+                //m_pwmController.SetDesiredFrequency(1000);
+                m_servo0 = m_pwmController.OpenPin(0);
+                m_servo1 = m_pwmController.OpenPin(1);
 
-                m_servo0.SetActiveDutyCyclePercentage(0.5);
-                m_servo0.Start();
-                m_servo1.SetActiveDutyCyclePercentage(0.5);
-                m_servo1.Start();
+                m_motor0 = m_pwmController.OpenPin(4);
+                m_motor1 = m_pwmController.OpenPin(5);
 
-                //m_timer = new DispatcherTimer();
-                //m_timer.Tick += M_timer_Tick;
-                //m_timer.Interval = TimeSpan.FromMilliseconds(500);
-                //m_timer.Start();
+                //m_servo0.SetActiveDutyCyclePercentage(0.5);
+                //m_servo0.Start();
+                //m_servo1.SetActiveDutyCyclePercentage(0.5);
+                //m_servo1.Start();
+
+                m_motor0.SetActiveDutyCyclePercentage(0.5);
+                m_motor0.Start();
+                m_motor1.SetActiveDutyCyclePercentage(0.5);
+                m_motor1.Start();
+
+                m_timer = new DispatcherTimer();
+                m_timer.Tick += M_timer_Tick;
+                m_timer.Interval = TimeSpan.FromMilliseconds(2500);
+                m_timer.Start();
             }
             catch (Exception ex)
             {
@@ -67,22 +76,25 @@ namespace DemoPCA9685
 
         }
 
-        const double STEP = 0.1;
-        double m_duty0 = 0, m_duty1 = 1, m_dir0 = +STEP, m_dir1 = -STEP;
-        const double MINDUTY = 0, MAXDUTY = 1;
+        bool m_switch = false;
         private void M_timer_Tick(object sender, object e)
         {
-            Debug.WriteLine($"{m_duty0}, {m_duty1}");
-            m_servo0.SetActiveDutyCyclePercentage(m_duty0);
-            m_servo1.SetActiveDutyCyclePercentage(m_duty1);
-            m_servo0.Start();
-            m_servo1.Start();
-            m_duty0 += m_dir0;
-            m_duty1 += m_dir1;
-            if (m_duty0 < MINDUTY) { m_duty0 = MINDUTY; m_dir0 = STEP; }
-            if (m_duty0 > MAXDUTY) { m_duty0 = MAXDUTY; m_dir0 = -STEP; }
-            if (m_duty1 < MINDUTY) { m_duty1 = MINDUTY; m_dir1 = STEP; }
-            if (m_duty1 > MAXDUTY) { m_duty1 = MAXDUTY; m_dir1 = -STEP; }
+            if (m_switch)
+            {
+                //m_servo0.SetActiveDutyCyclePercentage(0.5);
+                //m_servo1.SetActiveDutyCyclePercentage(0.5);
+
+                m_motor0.SetActiveDutyCyclePercentage(0.5);
+                m_motor1.SetActiveDutyCyclePercentage(0.5);
+            } else
+            {
+                //m_servo0.SetActiveDutyCyclePercentage(0.2);
+                //m_servo1.SetActiveDutyCyclePercentage(0.2);
+
+                m_motor0.SetActiveDutyCyclePercentage(0.999);
+                m_motor1.SetActiveDutyCyclePercentage(0.999);
+            }
+            m_switch = !m_switch;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 # 2016windowsiot-iothub-PWM-PCA9685-L298-motor-driver
 2016windowsiot-iothub-PWM-PCA9685-L298-motor-driver
 
+## Components
+* PCA9685 
+* [L298, GHI, .NET Gadgeteer](https://www.ghielectronics.com/catalog/product/315) (or similar)
+    * [P Socket, .NET Gadgeteer](http://gadgeteer.codeplex.com/wikipage?title=Socket%20Type%20P) 
+* 2 servos
+* 2 DC motors
+
+
+## Connections
+### I2C
+Connect PCA9685 to I2C
+
+### P Socket
+* P1 - +3.3V (not used)
+* P2 - +5V (connect to +5 on RPI)
+* P3, 
+
+
+### PWM from PCA9685
+1,2 -> Servo
+
+4 -> P8
+
+5 -> P7
+
+### GPIO
+-> P9 (direction)
+
+-> P6 (direction)
+
+### 5V
+On P socket pin 2 (marked as 5V). 3.3V - not needed
+
+### Others
+GND!
+
+DC Motor - 6V, PCA9685 + L298 - need 9V! for 0.99 = 1
+## Remarks
 Do not mix GNP and + in load - will not start
